d3dda4a7b609198e92e918db6da3fc1d6a805012
Riccardo <zottiriccardo08@gmail.com>
2026-05-27 10:49:59 +0200

    inserimento di due tuple nella tabella spettatore

---
+++ DB/popolaDB.sql
@@ -0,0 +1,5 @@
+insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`) 
+values ('Zotti', 'zottiriccardo08@gmail.com', 1, 'Riccardo', '1234');
+
+insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`) 
+values ('Leoni', 'lucaleoni08@gmail.com', 2, 'Luca', '5678');
