3325d6b4e734daa8c30b4f966a12617e7b776e5d
luca <lucaleoni08@gmail.com>
2026-06-04 10:13:33 +0200

    Aggiornati i valori di hashpw

--- DB/popolaDB.sql
+++ DB/popolaDB.sql
@@ -1,5 +1,5 @@
 insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`, `ruolo`) 
-values ('Zotti', 'zottiriccardo08@gmail.com', 1, 'Riccardo', '1234', 1);
+values ('Zotti', 'zottiriccardo08@gmail.com', 1, 'Riccardo', '$2b$12$Os6qE/nm5WK2ca0YhTXsMunyB2JSrvAnBocKiIH.XYTEh/.doXkkq', 1);
 
 insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`, `ruolo`) 
-values ('Leoni', 'lucaleoni08@gmail.com', 2, 'Luca', '5678', 1);
+values ('Leoni', 'lucaleoni08@gmail.com', 2, 'Luca', '$2b$12$JE3.k2lR/cr3xMtM9IjNouTncNEfNVPBR1jNEZMUqBb4w.qA6Lhb2', 1);
