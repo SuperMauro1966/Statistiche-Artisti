c8ff8631851e42bc708dece2ef376326dc304e75
luca <lucaleoni08@gmail.com>
2026-06-04 09:52:28 +0200

    Correzione da VARCHAR a TINYINT nel popolamento di spettatore

--- DB/popolaDB.sql
+++ DB/popolaDB.sql
@@ -1,5 +1,5 @@
-insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`) 
-values ('Zotti', 'zottiriccardo08@gmail.com', 1, 'Riccardo', '1234');
+insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`, `ruolo`) 
+values ('Zotti', 'zottiriccardo08@gmail.com', 1, 'Riccardo', '1234', 1);
 
-insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`) 
-values ('Leoni', 'lucaleoni08@gmail.com', 2, 'Luca', '5678');
+insert into `spettatore` (`cognome`, `email`, `id_spettatore`, `nome`, `password_hash`, `ruolo`) 
+values ('Leoni', 'lucaleoni08@gmail.com', 2, 'Luca', '5678', 1);
